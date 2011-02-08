cf9fa8c286274ff71467ba7d8b53a3b523878f77
Robbie Lamb <robbie.lamb@gmail.com>
2011-02-08 14:15:19 -0700

    Adding in a lua-mode

--- lamb/load.el
+++ lamb/load.el
@@ -4,3 +4,9 @@
 (require 'save-visited-files)
 (turn-on-save-visited-files-mode)
 
+                                        ; Add lua!
+(setq auto-mode-alist (cons '("\\.lua$" . lua-mode) auto-mode-alist))
+(autoload 'lua-mode "lua-mode" "Lua editing mode." t)
+
+(add-hook 'lua-mode-hook 'hs-minor-mode)
+
