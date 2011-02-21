363376493468a7c55f0fdef7067bb075955d6f38
Robbie Lamb <robbie.lamb@gmail.com>
2011-02-21 10:04:36 -0700

    Merging my org-mode config files.

--- lamb/org-mode-config.el
+++ lamb/org-mode-config.el
@@ -10,6 +10,11 @@
 (require 'org-bibtex)
 (require 'org-exp-bibtex)
 
+;; Some special keys for something
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
 
 ;; Export ical format using UTC time
 (setq org-icalendar-use-UTC-date-time t)
