97e964c69e1ff4917aea8ec62ddf6e913562e553
Robbie Lamb <robbie.lamb@gmail.com>
2011-03-25 18:27:57 -0600

    Load CMake syntax highlighting.

--- lamb/load.el
+++ lamb/load.el
@@ -1,12 +1,20 @@
 (require 'undo-tree)
 (global-undo-tree-mode)
 
-(require 'save-visited-files)
-(turn-on-save-visited-files-mode)
+;; (require 'save-visited-files)
+;; (turn-on-save-visited-files-mode)
 
-                                        ; Add lua!
+;; Add lua!
 (setq auto-mode-alist (cons '("\\.lua$" . lua-mode) auto-mode-alist))
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
 
 (add-hook 'lua-mode-hook 'hs-minor-mode)
+
+;; Cmake stuff
+
+(require 'cmake-mode)
+(setq auto-mode-alist
+      (append '(("CMakeLists\\.txt\\'" . cmake-mode)
+                ("\\.cmake\\'" . cmake-mode))
+              auto-mode-alist))
 
