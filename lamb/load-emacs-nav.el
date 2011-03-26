a64301fe829b6062d10b68d81dd16091359bde36
Robbie Lamb <robbie.lamb@gmail.com>
2011-03-25 18:27:26 -0600

    Use a newer nav.

--- lamb/load-emacs-nav.el
+++ lamb/load-emacs-nav.el
@@ -1,6 +1,6 @@
 ;; Load Emacs Nav
 
-(add-to-list 'load-path (concat dotfiles-dir "lamb/emacs-nav-20101227"))
+(add-to-list 'load-path (concat dotfiles-dir "lamb/emacs-nav-20110220"))
 (require 'nav)
 
 (define-key global-map "\C-cb" 'nav)
