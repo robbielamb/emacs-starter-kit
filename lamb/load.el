681485577204db4e5d9fc863db457001e5d79d1a
Robbie Lamb <robbie.lamb@gmail.com>
2011-04-09 13:30:44 -0600

    Adding in the autocomplete plugin.

--- lamb/load.el
+++ lamb/load.el
@@ -18,3 +18,22 @@
                 ("\\.cmake\\'" . cmake-mode))
               auto-mode-alist))
 
+(add-to-list 'load-path "~/.emacs.d/lamb/auto-complete")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/lamb/auto-complete/ac-dict")
+(ac-config-default)
+
+;; Auto Complete for C/C++ programs
+;; Requires the use of a .clang-completion-config.el file in the project.
+(require 'auto-complete-clang)
+(setq clang-completion-suppress-error 't)
+
+(defun my-c-mode-common-hook()
+  (setq ac-auto-start nil)
+  (setq ac-expand-on-auto-complete nil)
+  (setq ac-quick-help-delay 0.3)
+  (define-key c-mode-base-map (kbd "M-/") 'ac-complete-clang)
+)
+
+(add-hook 'c-mode-common-hook 'my-c-mode-common-hook)
+
